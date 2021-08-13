008be4d07afe6aa40c4043ef81d0b380e981946e
joricky91 <jonathanricky91@gmail.com>
2021-08-13 20:43:39 +0700

    finishing javascript for mobile

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.querySelector(".icon").addEventListener("click", function(){
+const iconClick = document.querySelector(".icon");
+iconClick.addEventListener("click", () => {
     document.querySelector(".share-button").style.display = "flex";
 });
+
+const iconClose = document.querySelector(".share-icon");
+iconClose.addEventListener("click", () => {
+    document.querySelector(".share-button").style.display = "none";
+})
